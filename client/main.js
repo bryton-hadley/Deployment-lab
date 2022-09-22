3bb4799b92ebe039344f10f450f7acd315f742d4
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-22 17:01:23 -0600

    rollbar finished!

--- client/main.js
+++ client/main.js
@@ -1,5 +1,72 @@
 
 
+//selecting the add item html element 
+const addComment = document.getElementById('addComment')
+const addForm = document.getElementById('addForm')
+
+// selecting the add item 
+const comment = document.getElementById('displayComment')
+
+
+
+// write a function for my axios request 
+
+function viewComment(res) {
+  container.innerHTML = ''
+  nameInput.value = ''
+
+  res.data.forEach((comment, index) => {
+      container.innerHTML += `<p name=${index}>${comment}</p>`
+  })
+
+  document.querySelectorAll('p').forEach(element => {
+      const theIndexValue = element.getAttribute('name');
+
+      element.addEventListener('click', () => {
+          axios
+              .delete(`/api/students/${theIndexValue}`)
+              .then(res => {
+                  putTheThingInTheView(res)
+              })
+      })
+  })
+}
+
+function submitComment(evt) {
+  evt.preventDefault();
+
+  axios
+      .post('/api/comments', { post: postInput.value })
+      .then(res => {
+          viewComment(res)
+      })
+      .catch(err => {
+          postInput.value = ''
+
+          const notif = document.createElement('aside')
+          notif.innerHTML = `<p>${err.response.data}</p>
+          <button class="close">close</button>`
+          document.body.appendChild(notif)
+
+          document.querySelectorAll('.close').forEach(btn => {
+              btn.addEventListener('click', (e) => {
+                  e.target.parentNode.remove()
+              })
+          })
+      })
+}
+
+// get student list on initial load
+axios
+  .get('/api/comments')
+  .then(res => {
+      putTheThingInTheView(res)
+  })
+  .catch((err) => {
+      console.log(err)
+  })
+
+addForm.addEventListener('submit', submitHandler)
   // function lightTheme() {
   //   imageSection.classList.remove("image-container");
 
--- server/index.js
+++ server/index.js
@@ -29,11 +29,47 @@ app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 
 app.use(express.static(path.join(__dirname, '../client')))
 
-try {
-    nonExistentFunction ();
-} catch (error) {
-    rollbar.error(error.message)
-}
+//setting an empty arr to put the comments 
+const comments = []
+
+app.get('/', (res, req) => {
+
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+app.get('/api/comments', (req, res) => {
+    res.status(200).send(comments)
+})
+app.post('/api/comments', (req, res) => {
+    
+    let {post} = req.body
+    comments.push(post)
+
+    try {
+        if (index === -1 && post !== '') {
+         rollbar.log('Your comment was added successfully')
+            comments.push(post)
+            res.status(200).send(comments)
+        } else if (post === ''){
+         rollbar.error('No comment was provided')
+            res.status(400).send('You must enter a name.')
+        } else {
+         rollbar.critical('comment is already in array')
+            res.status(400).send('That student already exists.')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+ })
+ 
+ app.delete('/api/comments/:index', (req, res) => {
+     const targetIndex = +req.params.index
+     
+     comments.splice(targetIndex, 1)
+ 
+     rollbar.info("Your comment was deleted")
+ 
+     res.status(200).send(comments)
+ })
 
 
 
