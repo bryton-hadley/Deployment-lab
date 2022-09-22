cc3f48f18601d3efd8a4083bfe352363ec64545a
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-22 14:25:38 -0600

    setting up rollbar

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
+
+
+  // function lightTheme() {
+  //   imageSection.classList.remove("image-container");
+
+  // }
+  
+  // function darkTheme() {
+  //   imageSection.classList.add("image-container");
+    
+  
+  // }
+  
+  // var lightThemeBtn = document.querySelector("#Lucario");
+  // var darkThemeBtn = document.querySelector("#clear");
+  
+  // lightThemeBtn.addEventListener("click", lightTheme);
+  // darkThemeBtn.addEventListener("click", darkTheme);
+  
+
 console.log('YOU ARE DE BEST')
--- server/index.js
+++ server/index.js
@@ -4,11 +4,25 @@ require('dotenv').config()
 
 const path = require('path')
 
+const cors = require('cors')
+
 const port = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '14fbe4c9eb76482ba6130112925169ca',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('YOU ARE DOING GREAT!')
 
 //THIS ENDPOINTS TO SEND ALL FILES 
 app.use('/', express.static(path.join(__dirname, '../client/index.html')))
